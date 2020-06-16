efa4b1445b9c07200f13ac6f9e86daa1e64b2fff
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2020-06-16 11:52:21 +0200

    remove duplicate of testRetrieveLastEmailBySubject

--- src/test/java/util/xcmailr/XcMailrApiTest.java
+++ src/test/java/util/xcmailr/XcMailrApiTest.java
@@ -4,16 +4,9 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.mail.MessagingException;
-
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import util.xcmailr.data.EmailAccount;
-import util.xcmailr.util.SendEmail;
 import util.xcmailr.util.SendRequest;
 
 public class XcMailrApiTest extends AbstractXcMailrApiTest
@@ -36,28 +29,5 @@ public class XcMailrApiTest extends AbstractXcMailrApiTest
         {
             Assert.assertEquals(validMinutes, matcher.group(1));
         }
-    }
-
-    @Test
-    public void testRetrieveLastEmailBySubject() throws MessagingException, IOException
-    {
-        final String subject = "Test";
-        final String textToSend = "Hi\nHow are you?)\nBye";
-        XcMailrApi.createTemporaryEmail(tempEmail);
-
-        final EmailAccount emailAccount = new EmailAccount(CREDENTIALS.smtpServerEmail(), CREDENTIALS.smtpServerLogin(),
-                                                           CREDENTIALS.smtpServerPassword(), CREDENTIALS.smtpServerHost(),
-                                                           CREDENTIALS.smtpServerPort(), false, true);
-
-        SendEmail.send(emailAccount, tempEmail, subject, textToSend);
-
-        String response = XcMailrApi.retrieveLastEmailBySubject(tempEmail, subject);
-        JsonObject message = new JsonParser().parse(response).getAsJsonArray().get(0).getAsJsonObject();
-
-        Assert.assertEquals(message.get("mailAddress").getAsString().replaceAll("\"", ""), tempEmail);
-        Assert.assertEquals(message.get("sender").getAsString().replaceAll("\"", ""), emailAccount.getEmail());
-        Assert.assertEquals(message.get("subject").getAsString().replaceAll("\"", ""), subject);
-        Assert.assertEquals(decodeAndNormalize(message.get("htmlContent").getAsString()).replaceAll("\"", ""), textToSend);
-        Assert.assertEquals(decodeAndNormalize(message.get("textContent").getAsString()).replaceAll("\"", ""), textToSend);
     }
 }
