6a335b7d1813d033e6a581a187a74fe187afb946
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2020-06-17 10:08:31 +0200

    rename tests

---
+++ src/test/java/util/xcmailr/AbstractXcmailrApiTest.java
@@ -0,0 +1,63 @@
+package util.xcmailr;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.UUID;
+
+import org.aeonbits.owner.ConfigFactory;
+import org.apache.http.client.ClientProtocolException;
+import org.junit.After;
+import org.junit.BeforeClass;
+
+import util.xcmailr.util.Credentials;
+import util.xcmailr.util.SendRequest;
+
+public class AbstractXcmailrApiTest extends AbstractTest
+{
+    protected static final Credentials CREDENTIALS = ConfigFactory.create(Credentials.class, System.getenv());
+
+    protected static final String validMinutes = "1";
+
+    protected static String tempEmail;
+
+    @BeforeClass
+    public static void configureApiToken() throws ClientProtocolException, IOException
+    {
+        tempEmail = randomEmail("test", "varmail.net");
+
+        final String apiToken = System.getenv("XCMAILR_TOKEN");
+        if (apiToken != null)
+        {
+            properties.put("xcmailr.apiToken", apiToken);
+        }
+        properties.put("xcmailr.temporaryMailValidMinutes", validMinutes);
+        savePropertiesAndApply();
+
+        SendRequest.login(CREDENTIALS.xcmailrEmail(), CREDENTIALS.xcmailrPassword());
+    }
+
+    @After
+    public void deleteTempEmail() throws ClientProtocolException, IOException
+    {
+        SendRequest.deleteTempEmail(tempEmail);
+    }
+
+    protected static String decodeAndNormalize(String text)
+    {
+        return new String(Base64.getDecoder().decode(text)).replaceAll(String.valueOf((char) 13), "");
+    }
+
+    protected static String randomEmail(String prefix, String domain)
+    {
+        final String uuid = UUID.randomUUID().toString();
+        final String data = uuid.replaceAll("-", "");
+        final StringBuilder sb = new StringBuilder(42);
+
+        sb.append(prefix);
+        sb.append(data.concat(data).substring(0, 12));
+        sb.append("@");
+        sb.append(domain);
+
+        return sb.toString().toLowerCase();
+    }
+}
--- src/test/java/util/xcmailr/EmailCreationApiTest.java
+++ src/test/java/util/xcmailr/EmailCreationApiTest.java
@@ -1,63 +1,33 @@
 package util.xcmailr;
 
 import java.io.IOException;
-import java.util.Base64;
-import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import org.aeonbits.owner.ConfigFactory;
-import org.apache.http.client.ClientProtocolException;
-import org.junit.After;
-import org.junit.BeforeClass;
+import org.junit.Assert;
+import org.junit.Test;
 
-import util.xcmailr.util.Credentials;
 import util.xcmailr.util.SendRequest;
 
-public class EmailCreationApiTest extends AbstractTest
+public class EmailCreationApiTest extends AbstractXcmailrApiTest
 {
-    protected static final Credentials CREDENTIALS = ConfigFactory.create(Credentials.class, System.getenv());
-
-    protected static final String validMinutes = "1";
-
-    protected static String tempEmail;
-
-    @BeforeClass
-    public static void configureApiToken() throws ClientProtocolException, IOException
+    @Test
+    public void testEmailCreated() throws IOException
     {
-        tempEmail = randomEmail("test", "varmail.net");
+        XcMailrApi.createTemporaryEmail(tempEmail);
+        Assert.assertTrue(SendRequest.emailExists(tempEmail));
+    }
 
-        final String apiToken = System.getenv("XCMAILR_TOKEN");
-        if (apiToken != null)
+    @Test
+    public void testEmailExpired() throws IOException
+    {
+        String response = XcMailrApi.createTemporaryEmail(tempEmail);
+
+        Pattern validMinutesPatters = Pattern.compile("<div class=\"email-validity-minutes\">(\\d)</div>");
+        Matcher matcher = validMinutesPatters.matcher(response);
+        while (matcher.find())
         {
-            properties.put("xcmailr.apiToken", apiToken);
+            Assert.assertEquals(validMinutes, matcher.group(1));
         }
-        properties.put("xcmailr.temporaryMailValidMinutes", validMinutes);
-        savePropertiesAndApply();
-
-        SendRequest.login(CREDENTIALS.xcmailrEmail(), CREDENTIALS.xcmailrPassword());
-    }
-
-    @After
-    public void deleteTempEmail() throws ClientProtocolException, IOException
-    {
-        SendRequest.deleteTempEmail(tempEmail);
-    }
-
-    protected static String decodeAndNormalize(String text)
-    {
-        return new String(Base64.getDecoder().decode(text)).replaceAll(String.valueOf((char) 13), "");
-    }
-
-    protected static String randomEmail(String prefix, String domain)
-    {
-        final String uuid = UUID.randomUUID().toString();
-        final String data = uuid.replaceAll("-", "");
-        final StringBuilder sb = new StringBuilder(42);
-
-        sb.append(prefix);
-        sb.append(data.concat(data).substring(0, 12));
-        sb.append("@");
-        sb.append(domain);
-
-        return sb.toString().toLowerCase();
     }
 }
